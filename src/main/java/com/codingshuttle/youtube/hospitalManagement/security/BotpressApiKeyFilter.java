4d8c15045c173d3c98d25b9d923ec49e5b3c77f9
bessafi <bessafik@gmx.com>
2025-09-05 16:56:32 +0100

    Update BotpressApiKeyFilter.java

--- src/main/java/com/codingshuttle/youtube/hospitalManagement/security/BotpressApiKeyFilter.java
+++ src/main/java/com/codingshuttle/youtube/hospitalManagement/security/BotpressApiKeyFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
