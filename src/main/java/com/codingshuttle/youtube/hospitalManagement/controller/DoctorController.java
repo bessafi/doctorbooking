8ccbd64cdd02e5d4087754202a5a55d33e274451
bessafi <bessafik@gmx.com>
2025-09-08 20:49:56 +0100

    Update DoctorController.java

--- src/main/java/com/codingshuttle/youtube/hospitalManagement/controller/DoctorController.java
+++ src/main/java/com/codingshuttle/youtube/hospitalManagement/controller/DoctorController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/doctors")
+@RequestMapping("/doctors")
 @RequiredArgsConstructor
 public class DoctorController {
 
