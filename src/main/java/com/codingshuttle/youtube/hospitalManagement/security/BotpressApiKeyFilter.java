fe24855045ac68f8083664547ed98b8bcbc045c9
bessafi <bessafik@gmx.com>
2025-09-10 01:16:51 +0100

    Update BotpressApiKeyFilter.java add import to fix errors

--- src/main/java/com/codingshuttle/youtube/hospitalManagement/security/BotpressApiKeyFilter.java
+++ src/main/java/com/codingshuttle/youtube/hospitalManagement/security/BotpressApiKeyFilter.java
@@ -14,6 +14,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.List;
 
 import java.io.IOException;
 /* 
